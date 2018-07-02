916395ca9294dd93c325cc5d37cc10f634205f1e
Sebastian Boerakker <basti@boerakker.com>
2018-07-02 23:39:38 +0200

    add zsh aliases

---
+++ .config/zsh/common-aliases.zsh
@@ -0,0 +1,20 @@
+
+# ZSH
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
+
+
+# Pywal
+alias wal-wood="wal -q -n -a "92" -i ~/Pictures/Wallpapers/wood.jpg"
+alias wal-sunset="wal -q -n -a "92" -i ~/Pictures/Wallpapers/sunset.jpg"
+
+
+# Exa 
+alias ls="exa"
+alias la="exa -lah"
+alias l="exa -lah"
+alias lt="exa -Th"
+alias lg="exa -lh --git"
+
+# dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
