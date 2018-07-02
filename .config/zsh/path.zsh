b824eb2031a9cd255e1c16f29a34d1db91bba42c
Sebastian Boerakker <basti@boerakker.com>
2018-07-02 23:37:17 +0200

    add zsh configs

---
+++ .config/zsh/path.zsh
@@ -0,0 +1,7 @@
+export GOPATH=$HOME/go
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
+export VISUAL=vim
+export EDITOR="$VISUAL"
---
+++ .config/zsh/variables.zsh
@@ -0,0 +1,7 @@
+export VISUAL=vim
+export EDITOR=$VISUAL
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export TERM="xterm-256color"
+
+
