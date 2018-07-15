cf2a24e45c8342555ebaec5174b72785426db610
Sebastian Boerakker <basti@boerakker.com>
2018-07-15 14:08:52 +0200

    add polyconfig alias

--- .config/zsh/common-aliases.zsh
+++ .config/zsh/common-aliases.zsh
@@ -5,10 +5,9 @@ alias ohmyzsh="mate ~/.oh-my-zsh"
 
 
 # Pywal
-alias wal-wood="wal -q -n -a "92" -i ~/Pictures/Wallpapers/wood.jpg"
-alias wal-sunset="wal -q -n -a "92" -i ~/Pictures/Wallpapers/sunset.jpg"
+alias wal-wood="wal -q -n -a "92" -i ~/Pictures/Wallpapers/Backslide/wood.jpg"
+alias wal-sunset="wal -q -n -a "92" -i ~/Pictures/Wallpapers/Backslide/sunset.jpg"
 alias wal-yosemite="wal -n -a "92" -i ~/Pictures/Wallpapers/BackSlide/Yosemite-wallpaper-02-ShockBlast.jpg"
-
 
 # Exa 
 alias ls="exa"
@@ -25,6 +24,7 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias lock='/usr/bin/betterlockscreen -l'
 alias sound='alsamixer'
 alias i3config='vim ~/.config/i3/config'
+alias polyconfig='vim ~/.config/polybar/config'
 
 #
 alias vim='nvim'
