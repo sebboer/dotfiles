fd43f3e6f2f196c0d7110518d76170e538d78de6
Sebastian Boerakker <basti@boerakker.com>
2018-07-15 14:05:54 +0200

    show number of connected devices

--- .config/polybar/scripts/isactive-bluetooth.sh
+++ .config/polybar/scripts/isactive-bluetooth.sh
@@ -1,7 +1,3 @@
-#!/bin/sh
-
-if [ "$(systemctl is-active bluetooth.service)" = "active" ]; then
-	echo ""
-else
-	echo ""
-fi
+#!/bin/bash 
+OUTPUT=$(bt-device -l | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | xargs -L 1 bt-device -i | grep "Connected: 1" | wc -l)
+echo ' '$OUTPUT
