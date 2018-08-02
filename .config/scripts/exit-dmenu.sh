d643d80339dc50d6d90caed01ec360880df016c7
Sebastian Boerakker <basti@boerakker.com>
2018-08-02 22:40:49 +0200

    add hibernate entry

---
+++ .config/scripts/exit-dmenu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+action=$(printf "| lock\n| suspend\n| logout\n| hibernate\n| reboot\n| shutdown" | rofi -dmenu -i -p "Power Menu " | awk '{ print $2}')
+[[ "$action" = "" ]] && exit 1
+yn=$(echo -e "No\nYes" | rofi -dmenu -i -p "really $action?")
+[[ "$yn" = Yes ]] && ~/.config/scripts/i3exit $action
