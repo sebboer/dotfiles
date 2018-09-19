44b6d918ebd3380ff0f2c10b02dc8cb41628f42f
Sebastian Boerakker <basti@boerakker.com>
2018-09-19 20:17:12 +0200

    add java path

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -6,3 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
+
+# JAVA 
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk/
+export PATH=$PATH:/usr/jvm/java-8-openjdk/bin
