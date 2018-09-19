370971787ecbfbfe142aba47e924dc55a60c483e
Sebastian Boerakker <basti@boerakker.com>
2018-09-19 20:15:10 +0200

    update redshift script

--- .config/polybar/scripts/red.sh
+++ .config/polybar/scripts/red.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-if [[ $(cat ~/.config/polybar/scripts/.red-cache) -eq 1 ]]; then
-currenttime=$(date +%H)
-if [[ "$currenttime" -ge "21" ]] || [[ "$currenttime" -le "06" ]]; 
+if pidof redshift &> /dev/null;
 then
-	
-	if [[ $(pidof redshift | wc -w) -eq 0 ]];
-	then
-		redshift -c /home/basti/.config/redshift.conf &
-	fi
 	echo ""
 else
-	killall redshift &
-	echo ""
-fi
-else
-	killall redshift &
 	echo ""
 fi
