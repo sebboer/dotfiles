7fd9e2921c12bdd10ef908d93259b1ee61a611b0
Sebastian Boerakker <basti@boerakker.com>
2018-09-19 20:12:54 +0200

    update aliases

--- .config/zsh/common-aliases.zsh
+++ .config/zsh/common-aliases.zsh
@@ -10,8 +10,9 @@ alias wal-sunset="wal -q -n -a "92" -i ~/Pictures/Wallpapers/sunset.jpg"
 alias wal-yosemite="wal -n -a "92" -i ~/Pictures/Wallpapers/MacOS/Yosemite-wallpaper-02-ShockBlast.jpg"
 
 # Exa 
-alias ls="exa"
+alias ls="exa --across"
 alias la="exa -lah"
+alias ll="exa -l"
 alias l="exa -lah"
 alias lt="exa -Th"
 alias lg="exa -lh --git"
@@ -25,6 +26,7 @@ alias lock='/usr/bin/betterlockscreen -l'
 alias sound='alsamixer'
 alias i3config='vim ~/.config/i3/config'
 alias polyconfig='vim ~/.config/polybar/config'
+alias i3alias='ack --pager=less ^bindsym ~/.config/i3/config'
 
 # Docker
 alias dk='docker'
@@ -40,3 +42,4 @@ alias startdocker='systemctl start docker'
 alias vim='nvim'
 alias gnucash='LANGUAGE=de_DE.UTF-8 LANG=de_DE.UTF-8 LC_ALL=de_DE.UTF-8 gnucash'
 alias gnome-ctl='env XDG_CURRENT_DESKTOP=GNOME gnome-control-center'
+alias firefox='GTK_THEME="Adwaita" /usr/bin/firefox'
