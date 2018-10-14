cd1473fd303c2b3c4cff78335a9be767898ceb12
Sebastian Boerakker <basti@boerakker.com>
2018-10-14 16:15:44 +0200

    add install scripts

---
+++ .config/scripts/install.sh
@@ -0,0 +1,1 @@
+yay -S betterlockscreen google-chrome discord telegram-desktop postman-bin jetbrains-toolbox bitwarden-bin spotify playerctl mongodb-compass visual-studio-code-bin
---
+++ .config/scripts/pacman-install.sh
@@ -0,0 +1,1 @@
+pacman -S zsh urxvt i3-gaps polybar rofi neovim calibre thunderbird qtpass blueman veracrypt redshift gpa htop ranger feh
