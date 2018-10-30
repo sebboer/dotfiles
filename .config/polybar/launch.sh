30f8a2e3abad934c74c32f96c568bc3da850aec0
Sebastian Boerakker <basti@boerakker.com>
2018-10-30 01:01:49 +0100

    add bottom toolbar for 2nd display

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,8 +9,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch the bar on each Monitor
 if type "xrandr"; then
   for p in $(xrandr --query | grep " connected primary" | cut -d" " -f1);do
-    MONITOR=$p polybar --reload bottom &
     MONITOR=$p polybar --reload top &
+    MONITOR=$p polybar --reload bottom &
+  done
+  for p2 in $(xrandr --query | grep " connected " | grep -v "primary" | cut -d" " -f1);do
+    MONITOR=$p2 polybar --reload bottom2 &
   done
 else
   polybar --reload example &
