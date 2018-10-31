88894a530876642f24abb36903a67168c3ab3a77
Sebastian Boerakker <basti@boerakker.com>
2018-10-31 10:37:14 +0100

    add psql drivers for gnucash

--- .config/scripts/pacman-install.sh
+++ .config/scripts/pacman-install.sh
@@ -1,1 +1,1 @@
-pacman -S zsh rxvt-unicode urxvt-perls i3-gaps polybar rofi neovim calibre thunderbird qtpass blueman veracrypt redshift gpa htop ranger feh yay grive
+pacman -S zsh rxvt-unicode urxvt-perls i3-gaps polybar rofi neovim calibre thunderbird qtpass blueman veracrypt redshift gpa htop ranger feh yay grive libdbi-drivers
