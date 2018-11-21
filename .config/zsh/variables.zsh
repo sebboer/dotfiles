feaf6f0338fac4d75eb37b740b8ac48969158c2c
Sebastian Boerakker <basti@boerakker.com>
2018-11-21 13:19:44 +0100

    add fzf var for detecting hidden directories

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -5,3 +5,4 @@ export LC_ALL="en_US.UTF-8"
 export TERM="xterm-256color"
 export XDG_CONFIG_HOME=$HOME/.config/
 export XDG_CURRENT_DESKTOP="kde"
+export FZF_DEFAULT_COMMAND='ag --hiden --ignore .git -l -g ""'
