219e4f67bc2fbde249c9207f0c204abef04a12f0
Sebastian Boerakker <basti@boerakker.com>
2019-01-04 17:41:15 +0100

    handle timeout for update notifier

--- .config/polybar/scripts/update-notifier.sh
+++ .config/polybar/scripts/update-notifier.sh
@@ -1,1 +1,13 @@
-yay -Qu | wc -l
+#!/bin/bash
+
+query=`yay -Qu`
+if [[ $query == *"Get https://aur.archlinux.org"* ]]; then
+	echo "-"
+else
+	if [[ -z "$query"  ]]; then
+		echo "0"
+	else
+		echo $query | wc -l
+	fi
+#	yay -Qu | wc -l
+fi
