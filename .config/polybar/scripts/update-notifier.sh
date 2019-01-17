30629db7b93576212d5e881b165c6ea014d97256
Sebastian Boerakker <basti@boerakker.com>
2019-01-17 14:52:44 +0100

    redirect error for update script

--- .config/polybar/scripts/update-notifier.sh
+++ .config/polybar/scripts/update-notifier.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-query=`yay -Qu` 2> /dev/null || echo !
-if [[ $query == *"Get https://aur.archlinux.org"* ]]; then
-	echo "-"
-else
-	if [[ -z "$query"  ]]; then
-		echo "0"
-	else
-		echo $query | wc -l
-	fi
-#	yay -Qu | wc -l
-fi
+echo $(yay -Qu 2> /dev/null | wc -l)
+
