2976e1dcec665350e2befc8315a436271d3afefc
Sebastian Boerakker <basti@boerakker.com>
2019-01-18 17:55:02 +0100

    add shebang

--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 yay -S betterlockscreen-git google-chrome discord telegram-desktop postman-bin jetbrains-toolbox bitwarden-bin spotify playerctl mongodb-compass visual-studio-code-bin nerd-fonts-complete
--- .config/scripts/webcam.sh
+++ .config/scripts/webcam.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 ffmpeg -f v4l2 -vcodec h264 -i /dev/video0 -r 30 -f pulse -i default ~/Videos/out.mkv
