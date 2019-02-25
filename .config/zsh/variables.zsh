d89e4840d1ca3767484159b30ac89620241ff963
Sebastian Boerakker <basti@boerakker.com>
2019-02-25 16:01:45 +0100

    fix typo

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -5,4 +5,4 @@ export LC_ALL="en_US.UTF-8"
 export TERM="rxvt-unicode-256color"
 export XDG_CONFIG_HOME=$HOME/.config/
 export XDG_CURRENT_DESKTOP="kde"
-export FZF_DEFAULT_COMMAND='ag --hiden --ignore .git -l -g ""'
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -l -g ""'
