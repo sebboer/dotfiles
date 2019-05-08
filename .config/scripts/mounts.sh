1fb3b0e8877df9ced2eb6b5796076cdab67da277
Sebastian Boerakker <basti@boerakker.com>
2019-05-08 03:50:25 +0200

    add notify-send for mount script

--- .config/scripts/mounts.sh
+++ .config/scripts/mounts.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 
 mountpoint=$(lsblk -Jp | jq '.blockdevices[] | select(.children!=null) | .children[] | select(.type=="part") | select(.mountpoint==null) | select(.size|endswith("G")) | [.name, .size, .label] | join(" ")' -r | rofi -dmenu -i -p "Mount Disk" | awk '{print $1}')
 
 [[ "$mountpoint" = "" ]] && exit 1
-where=$(exa -l ~/mnt | awk '{print $7}' | rofi -dmenu  -i -p)
+mountdirs=$(exa -l ~/mnt | awk '{print $7}' | grep -v "nas")
+where=$(echo $mountdirs | rofi -dmenu  -i -p "Dirs to mount")
 [[ "$where" = "" ]] && exit 1
 where=~/mnt/$where
+if [[ ! -d "$where" ]]; then
+	mkdiryn=$(echo -e "No\nYes" | rofi -dmenu -i -p "$where does not exist. Create it?")
+	[[ "$mkdiryn" = Yes ]] && mkdir "$where"
+fi
 
-sudo mount $mountpoint $where 
+sudo mount $mountpoint $where && notify-send -a Mounter "$mountpoint mounted to $where."
