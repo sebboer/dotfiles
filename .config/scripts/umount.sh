3e24c5f47c9b18c4df7ecfade483f88a457282bc
Sebastian Boerakker <basti@boerakker.com>
2019-05-08 21:26:49 +0200

    add line limit

--- .config/scripts/umount.sh
+++ .config/scripts/umount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-umount=$(lsblk -Jp | jq '.blockdevices[] | select(.children!=null) | .children[] | select(.mountpoint!=null) | select(.mountpoint|startswith("'$HOME'")) | [.name, .mountpoint, .label, .size] | join(" ")'  -r | rofi -dmenu -i -p "Umount Disk" | awk '{print $2}')
+umount=$(lsblk -Jp | jq '.blockdevices[] | select(.children!=null) | .children[] | select(.mountpoint!=null) | select(.mountpoint|startswith("'$HOME'")) | [.name, .mountpoint, .label, .size] | join(" ")'  -r | rofi -dmenu -i -l 10 -p "Umount Disk" | awk '{print $2}')
 [[ "$umount" = "" ]] && exit 1
 
 sudo umount $umount && notify-send -a Mounter $umount umount
