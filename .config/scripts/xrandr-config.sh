5510d523be751b853ed51e745221851facfb7b01
Sebastian Boerakker <basti@boerakker.com>
2019-06-18 14:52:39 +0200

    remove 2nd monitor

--- .config/scripts/xrandr-config.sh
+++ .config/scripts/xrandr-config.sh
@@ -1,2 +1,3 @@
  #!/bin/bash
-xrandr --output DP-2 --auto --primary --output DP-0 --rotate left --left-of DP-2
+#xrandr --output DP-2 --auto --primary --output DP-0 --rotate left --left-of DP-2
+xrandr --output DP-2 --auto --primary --output DP-0 --off
