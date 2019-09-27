c8b47ce74cd3f42c6f53fa5268f7a02ef2ab665b
Sebastian Boerakker <basti@boerakker.com>
2019-09-27 15:04:35 +0200

    update install scripts

--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S betterlockscreen-git google-chrome discord telegram-desktop postman-bin jetbrains-toolbox bitwarden-bin spotify playerctl mongodb-compass visual-studio-code-bin nerd-fonts-complete
+yay -S betterlockscreen-git google-chrome discord telegram-desktop postman-bin jetbrains-toolbox bitwarden-bin spotify playerctl visual-studio-code-bin nerd-fonts-complete
--- .config/scripts/pacman-install.sh
+++ .config/scripts/pacman-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S zsh rxvt-unicode urxvt-perls i3-gaps polybar rofi neovim calibre thunderbird qtpass blueman veracrypt redshift gpa htop ranger feh yay grive libdbi-drivers
+pacman -S zsh i3-gaps polybar rofi neovim calibre thunderbird qtpass blueman veracrypt redshift gpa htop ranger feh yay grive
