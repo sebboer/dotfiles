5394b136284ddca56ed41767872627f3cbce4cdc
Sebastian Boerakker <basti@boerakker.com>
2019-10-01 00:54:04 +0200

    update install script

--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S betterlockscreen-git google-chrome discord telegram-desktop postman-bin jetbrains-toolbox bitwarden-bin spotify playerctl visual-studio-code-bin nerd-fonts-complete
+yay -S betterlockscreen-git google-chrome discord telegram-desktop postman-bin jetbrains-toolbox bitwarden-bin spotify playerctl visual-studio-code-bin nerd-fonts-complete htop-vim-git zathura zathura-pdf-mupdf sxiv lf youtube-dl homebank mpv gparted gotop jq lampe signal-desktop-bin hexyl compton-tryon-git nnn vim-plug
