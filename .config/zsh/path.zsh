e918b3eb6e417e6568123928cd9e2369a2221ff0
Sebastian Boerakker <basti@boerakker.com>
2019-10-22 20:38:34 +0200

    add go bin path

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -3,6 +3,7 @@ export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.config/scripts
 
 export GOPATH=$HOME/go
+export PATH=$PATH:$HOME/go/bin
 
 # JAVA
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
