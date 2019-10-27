5bdf3adde0bbf38fea2193dd894f21064881aa42
Sebastian Boerakker <basti@boerakker.com>
2019-10-27 17:06:08 +0100

    update nnn ENVARs

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -9,9 +9,7 @@ export XDG_CURRENT_DESKTOP="kde"
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -l -g ""'
 
 # NNN
-export DISABLE_FILE_OPEN_ON_NAV=1
-export NNN_RESTRICT_NAV_OPEN=1
 export NNN_COPIER=$HOME/.config/nnn/copier.sh
 export NNN_CONTEXT_COLORS='3254'
-export NNN_OPS_PROG=1
 export NNN_BMS='j:~/Downloads/JD;g:~/googledrive;m:~/mnt;s:~/googledrive/Studium;d:~/Downloads;p:~/Pictures;w:~/workspace;v:~/Videos;'
+export NNN_PLUG='s:sxiv;p:pdfview'
