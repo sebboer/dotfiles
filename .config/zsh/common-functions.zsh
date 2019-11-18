8d7ae8b51802d5ef6799671c1b85a0ece9972e74
Sebastian Boerakker <basti@boerakker.com>
2019-11-18 15:26:08 +0100

    rm todoist functions

--- .config/zsh/common-functions.zsh
+++ .config/zsh/common-functions.zsh
@@ -74,11 +74,3 @@ wget-recursive() {
 	wget --recursive --no-parent -e robots=off $1
 }
 
-todoistkeys() {
-	echo "<C-x> t t: select task with peco
-<C-x> t p: select project with peco
-<C-x> t l: select labels with peco
-<C-x> t c: select task and close with peco
-<C-x> t d: select date
-<C-x> t o: select task, and open it with browser when has url"
-}
