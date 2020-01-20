58df5ca2ccdc9398e3e830d7634de0744bece27f
Sebastian Boerakker <basti@boerakker.com>
2020-01-20 10:28:36 +0100

    add error notification to umount script

--- .config/scripts/mount/umount.sh
+++ .config/scripts/mount/umount.sh
@@ -3,5 +3,12 @@
 umount=$(lsblk -Jp | jq '.blockdevices[] | select(.children!=null) | .children[] | select(.mountpoint!=null) | select(.mountpoint|startswith("'$HOME'")) | [.name, .mountpoint, .label, .size] | join(" ")'  -r | rofi -dmenu -i -l 10 -p "Umount Disk" | awk '{print $2}')
 [[ "$umount" = "" ]] && exit 1
 
-sudo umount $umount && notify-send -a Mounter "$umount umount" -i hdd
+ERROR=$(  sudo umount $umount 2>&1 )
+
+if [ $? -eq 0 ]; then
+	notify-send -i hdd -a Mounter "$umount umount"
+else
+	notify-send -i hdd -u critical -a Mounter "$ERROR"
+fi
+
 
