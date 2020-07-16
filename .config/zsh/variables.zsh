51f2d1a8c9ea8c95c41a3692e7133392a17d3aea
Sebastian Boerakker <basti@boerakker.com>
2020-07-16 18:43:15 +0200

    update nnn bookmarks

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -11,7 +11,7 @@ export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -l -g ""'
 # NNN
 export NNN_COPIER=$HOME/.config/nnn/copier.sh
 export NNN_CONTEXT_COLORS='3254'
-export NNN_BMS='j:~/Downloads/JD;m:~/mnt;d:~/Downloads;p:~/Pictures;w:~/workspace;v:~/Videos;t:~/.local/share/Trash;c:~/.config;n:~/Nextcloud;s:~/Nextcloud/Documents/Studium'
+export NNN_BMS='j:~/Downloads/JD;m:~/mnt;d:~/Downloads;p:~/Pictures;w:~/workspace;v:~/Videos;t:~/.local/share/Trash;c:~/.config;n:~/NextCloud;s:~/NextCloud/Documents/Studium'
 export NNN_PLUG='s:sxiv;p:pandoc;b:bat;m:mediainf;l:less;h:hexylview;i:imgview;t:preview-tui'
 export NNN_TRASH=1
 export PAGER=less
