e23fe243fd6c7b27f9b3c381d1bf09ea46bc8ed1
Sebastian Boerakker <basti@boerakker.com>
2020-11-12 21:35:46 +0100

    update nnn plugins and bookmarks

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -1,5 +1,6 @@
 export VISUAL=/usr/bin/nvim
 export EDITOR=$VISUAL
+export BROWSER=firefox-developer-edition
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 #export TERM="rxvt-unicode-256color"
@@ -11,8 +12,8 @@ export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -l -g ""'
 # NNN
 export NNN_COPIER=$HOME/.config/nnn/copier.sh
 export NNN_CONTEXT_COLORS='3254'
-export NNN_BMS='j:~/Downloads/JD;m:~/mnt;d:~/Downloads;p:~/Pictures;w:~/workspace;v:~/Videos;t:~/.local/share/Trash;c:~/.config;n:~/NextCloud;s:~/NextCloud/Documents/Studium'
-export NNN_PLUG='s:sxiv;p:pandoc;b:bat;m:mediainf;l:less;h:hexylview;i:imgview;t:preview-tui'
+export NNN_BMS='j:~/Downloads/JD;m:~/mnt;d:~/Downloads;p:~/Pictures;w:~/workspace;v:~/Videos;t:~/.local/share/Trash;c:~/.config;n:~/Nextcloud;s:~/Nextcloud/Documents/Studium;b:/run/media/basti/BACKUP'
+export NNN_PLUG='s:sxiv;p:pandoc;b:bat;m:mediainfo;l:less;h:hexylview;i:imgview;t:preview-tui'
 export NNN_TRASH=1
 export PAGER=less
 export GIT_PAGER=bat
